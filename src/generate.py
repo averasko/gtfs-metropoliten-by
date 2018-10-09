40e55a54da369054c61b5a274634fa5837e910aa
Aleh Veraskouski <aleh.veraskouski@gmail.com>
2018-10-09 20:04:10 +0300

    adding extract_freq() function

--- src/generate.py
+++ src/generate.py
@@ -5,6 +5,18 @@ TEST_URL = "file:../input/schedule.html"
 URL = "http://metropoliten.by/information/schedule/"
 
 
+def extract_freq(s):
+  s = s.replace(",", ".")
+  if re.search("-", s):
+    pattern = "([\.0-9]*)-([\.0-9]*)"
+    s_ab = re.findall(pattern, s)
+    s_from = float(s_ab[0][0])
+    s_to = float(s_ab[0][1])
+  else:
+    s_from = float(s)
+    s_to = s_from
+  return (s_from, s_to)
+
 
 def extract_schedule(url = TEST_URL):
   f = urllib.urlopen(url)
@@ -16,4 +28,7 @@ def extract_schedule(url = TEST_URL):
   print(result)
 
 
-extract_schedule()
+#extract_schedule()
+print(extract_freq("5,0-7.0"))
+print(extract_freq("2,2"))
+print(extract_freq("5-2"))
