4d06f25130689fb0b1709f2c08deded5c05a1c98
Aleh Veraskouski <aleh.veraskouski@gmail.com>
2018-10-11 15:22:49 +0300

    adding time parsing into h:m-h:m

--- src/generate.py
+++ src/generate.py
@@ -7,16 +7,20 @@ URL = "http://metropoliten.by/information/schedule/"
 
 def extract_freq(s):
   s = s.replace(",", ".")
-  if re.search("-", s):
-    pattern = "([\.0-9]*)-([\.0-9]*)"
-    s_ab = re.findall(pattern, s)
-    s_from = float(s_ab[0][0])
-    s_to = float(s_ab[0][1])
+  if re.match("[\.0-9]+-[\.0-9]+", s) or re.match("[\.0-9]+", s):
+    if re.search("-", s):
+      pattern = "([\.0-9]+)-([\.0-9]+)"
+      s_ab = re.findall(pattern, s)
+      s_from = float(s_ab[0][0])
+      s_to = float(s_ab[0][1])
+    else:
+      s_from = float(s)
+      s_to = s_from
+    return (s_from, s_to)
   else:
-    s_from = float(s)
-    s_to = s_from
-  return (s_from, s_to)
-
+    # note: obviously, that was not a clean check
+    # TODO: fix that
+    return None
 
 def extract_schedule(url = TEST_URL):
   f = urllib.urlopen(url)
@@ -24,11 +28,51 @@ def extract_schedule(url = TEST_URL):
   #result = re.findall("<td.*\n.*<p.*[0-9]{2}.[0-9]{2}-[0-9]{2}.[0-9]{2}.*</p", myfile)
   elem = ".*<td.*\n.*>(.*)<o.*\n"
   pattern = elem + elem + elem + elem + elem
-  result = re.findall(pattern, myfile)
-  print(result)
+  return re.findall(pattern, myfile)
+
+def extract_time(s):
+  pattern = "([0-9]+)\.([0-9]+)-([0-9]+)\.([0-9]+)"
+  #had to adjust regex due to incorrect data on site: '09.00-10-00'
+  pattern = "([0-9]+)\.([0-9]+)-([0-9]+)[\.-]([0-9]+)"
+  t_ab = re.findall(pattern, s)
+  t_from_h = int(t_ab[0][0])
+  t_from_m = int(t_ab[0][1])
+  t_to_h = int(t_ab[0][2])
+  t_to_m = int(t_ab[0][3])
+  return (t_from_h, t_from_m, t_to_h, t_to_m)
+
+def apply_on_column(index, func, interval):
+  res = []
+  for i, col in enumerate(interval):
+    if i == index:
+      res.append(func(col))
+    else:
+      res.append(col)
+  return res
+
+def apply_extract_time_on_first_column(interval):
+  return apply_on_column(0, extract_time, interval)
+
+def apply_on_column_in_list(index, func, intervals):
+  return list(map(apply_extract_time_on_first_column, intervals))
+
+def split_time(intervals):
+  return apply_on_column_in_list(0, extract_time, intervals)
 
 
-#extract_schedule()
-print(extract_freq("5,0-7.0"))
-print(extract_freq("2,2"))
-print(extract_freq("5-2"))
+
+
+intervals = extract_schedule()
+
+intervals = split_time(intervals)
+
+
+for interval in intervals:
+  print(interval)
+
+
+
+#print(extract_freq("5,0-7.0"))
+#print(extract_freq("2,2"))
+#print(extract_freq("5-2"))
+#print(extract_freq("garbage"))
